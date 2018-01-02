78a9e397b3f1a236f2b976c2d56d315be6022198
tuobaye0711 <tuobaye0711@163.com>
2018-01-02 19:55:47 +0800

    接口优化—增加查询N天前的壁纸，增加地区选择

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 /**
  * Created by 10202386 on 2017/12/28.
  */
-let express = require('express');
-let request = require('request');
-let app = express();
+const express = require('express');
+const request = require('request');
+const app = express();
 
 app.get('/', function (req, res) {
-    res.send('new wallpaper everyday!')
+    res.send('welcome to new-wallpaper-everyday!')
 });
-app.get('/today', getWallpaperToday);
+app.get('/wallpaper', getWallpaper);
 
 app.listen(8888, function () {
     console.log('Example app listening on port 8888!');
@@ -17,13 +17,33 @@ app.listen(8888, function () {
 function getUri(start, number, mkt) {
     return 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=' + start + '&n=' + number + '&mkt=' + mkt
 }
-function getWallpaperToday(req, res) {
-    let uri = getUri(0, 1, 'zh-CN');
+// function getWallpaperToday(req, res) {
+//     let uri = getUri(0, 1, 'zh-CN');
+//     request(uri, function (error, response, body) {
+//         if (!error && response.statusCode === 200) {
+//             let data = JSON.parse(body);
+//             let images = data.images;
+//             res.redirect('https://www.bing.com'+images[0].url)
+//         }else{
+//             res.send('request error!')
+//         }
+//     })
+// }
+
+function getWallpaper(req, res) {
+    let days_ago = req.query.days_ago || 0;
+    let mkt = req.query.mkt || 'zh-CN';
+    let uri;
+    if (days_ago <= 7){
+        uri = getUri(days_ago, 1, mkt)
+    }else {
+        uri = getUri(7, days_ago-6, mkt)
+    }
     request(uri, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             let data = JSON.parse(body);
             let images = data.images;
-            res.redirect('https://www.bing.com'+images[0].url)
+            res.redirect('https://www.bing.com'+images[images.length-1].url)
         }else{
             res.send('request error!')
         }
