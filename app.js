ad1c47e5ac1342ef4e1d24583ab025edf8be349a
tuobaye0711 <tuobaye0711@163.com>
2018-01-03 10:35:29 +0800

    优化首页

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('welcome to new-wallpaper-everyday!')
+    res.sendfile('index.html')
 });
 app.get('/wallpaper', getWallpaper);
 
